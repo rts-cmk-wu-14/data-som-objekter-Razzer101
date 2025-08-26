15c6a6a9399611c7641c01924d142f47fc3aa6ec
Razzer101 <Rasmus@anders-net.dk>
2025-08-26 13:02:24 +0200

    Header Done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
-// slå dig løs her... 
+const heroSection = document.querySelector(".hero")
+const heroHeadlineNewArray = hero.headline.split("save your time")
 
-//eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+const heroContent = /*html*/ `
+<figure>
+    <img src=${hero.image} alt="head image">
+    <figcaption class="heroFig">
+        <h1>${heroHeadlineNewArray[0]}<span class="orangeText">${hero.headline.slice(19, 34)}</span>${heroHeadlineNewArray[1]}</h1>
+        <p>${hero.copy}</p>
+        <button><img src=${hero.icon} alt="head image">Explore</button>
+    </figcaption>
+</figure>
+`
+
+heroSection.insertAdjacentHTML("afterbegin", heroContent)
