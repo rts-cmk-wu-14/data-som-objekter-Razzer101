a77749a8d31952974628ea06114c2619719e6c36
Razzer101 <Rasmus@anders-net.dk>
2025-08-26 13:43:53 +0200

    servicesDone

---
+++ js/services.js
@@ -0,0 +1,16 @@
+const servicesSection = document.querySelector(".services")
+
+const servicesContent = services.map((elm) => {
+    return /*html*/ `
+<article>
+    <figure>
+        <img src=${elm.illustration} alt="head image">
+    </figure>
+    <h3>${elm.headline}</h3>
+    <p>${elm.text}</p>
+    <a href="#">${elm.linktext}</a>
+</article>
+`
+}).join("")
+
+servicesSection.insertAdjacentHTML("afterbegin", servicesContent)
